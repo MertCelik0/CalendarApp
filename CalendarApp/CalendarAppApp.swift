2dfe6251771e0b0fd70d66a71eb6150f729b3858
Mert ÇELİK <celik.mert@homtail.com>
2021-09-21 23:33:55 +0300

    Initial Commit

---
+++ CalendarApp/CalendarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAppApp.swift
+//  CalendarApp
+//
+//  Created by Mert Çelik on 21.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
